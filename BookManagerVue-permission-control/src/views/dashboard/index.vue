41fcf685b11bb85b4f4d8e2f7337c9d9f7f6507c
ccsherryk <2931109213@qq.com>
2023-10-30 16:46:34 +0800

    “change2”

--- BookManagerVue-permission-control/src/views/dashboard/index.vue
+++ BookManagerVue-permission-control/src/views/dashboard/index.vue
@@ -3,10 +3,10 @@
     <div title="欢迎页面" style="padding: 50px; overflow: hidden; color: #409eff;font: lighter 16px 'Lucida Sans Unicode';"
          data-options="iconCls:'icon-heart',plain:true">
         <b style="font-size: 36px; line-height: 30px; height: 30px;">欢迎使用图书管理系统</b>
-        <p>开发人员: 江苏第二师范学院 数学与信息技术学院 王鹏</p>
-        <p>联系方式-Email: 894176237@qq.com</p>
-        <p>联系方式-QQ: 894176237</p>
-        <p>开发周期: 2021/11/8 —— 2021/11/13</p>
+        <p>开发人员:数据库大作业</p>
+        <p>联系方式-Email: </p>
+        <p>西安交通大学软件学院</p>
+        <p>开发周期: 2023/10/28 —— 2023/11/26</p>
         <hr/>
         <b>项目开发环境介绍</b>
         <p>操作系统: Windows 10 home</p>
